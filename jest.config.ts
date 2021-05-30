55db1320a3079f6838286787e238e8fcd07e7bae
Henrique Menna Barreto <henrique.menna@triider.com>
2021-05-30 01:22:27 -0300

    add coverage report

--- jest.config.ts
+++ jest.config.ts
@@ -5,7 +5,10 @@ const { compilerOptions } = require('./tsconfig.json');
 
 export default {
   clearMocks: true,
-  coverageProvider: 'v8',
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/modules/**/services/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text-summary', 'lcov'],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>/src/',
   }),
